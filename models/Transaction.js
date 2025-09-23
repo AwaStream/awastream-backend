7d7b699dbfa245f3e6bcb46858f7a66b2c5435c4
NaijaTechBro <bakisodiq@gmail.com>
2025-09-23 19:15:11 +0100

    update: implementing paystack

--- models/Transaction.js
+++ models/Transaction.js
@@ -5,8 +5,8 @@ const TransactionSchema = new mongoose.Schema({
     creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     video: { type: mongoose.Schema.Types.ObjectId, ref: 'Video', required: true },
     amountKobo: { type: Number, required: true }, // Gross amount paid by viewer
-    commissionKobo: { type: Number, required: true }, // AwaStream's 15% cut
-    creatorEarningsKobo: { type: Number, required: true }, // The 85% net for the creator
+    commissionKobo: { type: Number }, // AwaStream's 15% cut
+    creatorEarningsKobo: { type: Number }, // The 85% net for the creator
     status: {
         type: String,
         enum: ['pending', 'successful', 'failed'],
