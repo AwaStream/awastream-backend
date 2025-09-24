f0b99391848d7317c46a023d4e4352a3211d3ed7
NaijaTechBro <bakisodiq@gmail.com>
2025-09-24 23:09:32 +0100

    clean up

--- models/Video.js
+++ models/Video.js
@@ -1,32 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const VideoSchema = new mongoose.Schema({
-//     creator: { 
-//         type: mongoose.Schema.Types.ObjectId, 
-//         ref: 'User', 
-//         required: true 
-//     },
-//     youtubeUrl: { type: String, required: true }, 
-//     youtubeVideoId: { type: String, required: true },
-//     title: { type: String, required: true },
-//     description: { type: String },
-//     thumbnailUrl: { type: String, required: true },
-//     priceNaira: { type: Number, required: true },
-//     priceKobo: { type: Number, required: true, min: 15000 }, // Minimum price of 50 NGN
-//     shareableSlug: { type: String, required: true, unique: true },
-//     isActive: { type: Boolean, default: true },
-// }, { timestamps: true });
-
-// const Video = mongoose.model('Video', VideoSchema);
-// module.exports = Video;
-
-
-
-
-
-
-
-
 const mongoose = require('mongoose');
 
 const VideoSchema = new mongoose.Schema({
