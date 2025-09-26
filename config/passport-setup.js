3cf87e4b22bcaed86898c3af95c0c7b65feee993
NaijaTechBro <bakisodiq@gmail.com>
2025-09-26 14:36:52 +0100

    update: google authentication fix

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,73 +1,3 @@
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const User = require('../models/User');
-
-// passport.use(
-//     new GoogleStrategy(
-//         {
-//             clientID: process.env.GOOGLE_CLIENT_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//             callbackURL: '/api/v1/auth/google/callback',
-//             passReqToCallback: true,
-//         },
-//         async (req, accessToken, refreshToken, profile, done) => {
-//             const { id, displayName, emails, photos } = profile;
-//             const email = emails[0].value;
-//             const avatarUrl = photos[0].value;
-
-//             try {
-//                 let user = await User.findOne({ googleId: id });
-
-//                 if (user) {
-
-//                     user.lastLogin = new Date();
-//                     await user.save();
-//                     return done(null, user);
-//                 }
-
-//                 const existingEmailUser = await User.findOne({ email });
-//                 if (existingEmailUser) {
-//                     return done(new Error(`Email ${email} is already registered. Please log in with your original method.`), null);
-//                 }
-
-//                 const intent = req.session.intent; 
-
-//                 const newUser = await User.create({
-//                     googleId: id,
-//                     firstName: firstName,
-//                     lastName: lastName,
-//                     userName: userName,
-//                     email: email,
-//                     avatarUrl: avatarUrl,
-//                     authMethod: 'google',
-//                     isEmailVerified: true,
-//                     lastLogin: new Date(),
-//                     role: intent === 'viewer' ? 'viewer' : 'creator',
-//                 });
-
-//                 return done(null, newUser);
-//             } catch (error) {
-//                 return done(error, null);
-//             }
-//         }
-//     )
-// );
-
-// passport.serializeUser((user, done) => {
-//     done(null, user.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//     User.findById(id, (err, user) => done(err, user));
-// });
-
-
-
-
-
-
-
-
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
@@ -100,19 +30,18 @@ passport.use(
                 }
                 
                 // --- THIS IS THE FIX ---
-                // We must define our variables before using them.
+                // Provide a fallback of an empty string for the last name.
                 const firstName = name.givenName || 'User';
-                const lastName = name.familyName || '';
-                // Create a unique username from the email to prevent errors.
+                const lastName = name.familyName || ''; // If familyName is undefined, use ''
                 const userName = email.split('@')[0] + Math.floor(Math.random() * 1000);
                 
                 const intent = req.session.intent;
 
                 const newUser = await User.create({
                     googleId: id,
-                    firstName: firstName, // Now using the variable we defined above
-                    lastName: lastName,   // Now using the variable we defined above
-                    userName: userName,   // Now using the variable we defined above
+                    firstName: firstName,
+                    lastName: lastName, // This will now be an empty string instead of undefined
+                    userName: userName,
                     email: email,
                     avatarUrl: avatarUrl,
                     authMethod: 'google',
@@ -129,7 +58,6 @@ passport.use(
     )
 );
 
-// These functions are required by passport
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
